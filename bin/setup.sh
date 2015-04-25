a6e5527102d1c3b8cb1a597f51ca52e7c39ccd7b
Sascha Manns <Sascha.Manns@bdvb.de>
2015-04-25 14:14:53 +0200

    v0.1.0

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+echo "##################################################################################"
+echo "#                  PublicanCreators Setup                                        #"
+echo "##################################################################################"
 bundle install
 sudo apt-get install publican yad
 sudo add-apt-repository ppa:sascha-manns-h/publican -v
@@ -9,25 +12,43 @@ sudo apt-get update
 sudo apt-get install --only-upgrade publican
 if [ -e $HOME/.publicancreators.cfg ]
 then
-    echo "Do nothing"
+    echo "Found Config file"
 else
+    echo "Creating Config file"
     cp ../lib/PublicanCreators/.publicancreators.cfg $HOME
     if [ -e /usr/bin/gedit ]
     then
-        EDITOR = "gedit"
+        EDITOR="gedit"
     elif [ -e /usr/bin/kate ]
     then
-        EDITOR = "kate"
+        EDITOR="kate"
     elif [ -e /usr/bin/mousepad ]
     then
-        EDITOR = "mousepad"
+        EDITOR="mousepad"
     elif [ -e /usr/bin/geany ]
     then
-        EDITOR = "geany"
+        EDITOR="geany"
     elif [ -e /usr/bin/jedit ]
     then
-        EDITOR = "jedit"
+        EDITOR="jedit"
     fi
     $EDITOR $HOME/.publicancreators
 fi
+
+echo "Linking binary"
+FROM="$(pwd)"
+sudo ln -s $FROM/PublicanCreators.rb /usr/bin/publicancreators
+
+echo "Creating Desktop file"
+sudo cat <<EOF > $HOME/.local/share/applications/publicancreators.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=PublicanCreators
+Exec=/usr/bin/publicancreators
+Icon=$FROM/publican.png
+EOF
+
+
+
 # Do any other automated setup that you need to do here
