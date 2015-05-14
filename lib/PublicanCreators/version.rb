42015d6413443629690b9d2212000517406e690a
Sascha Manns <Sascha.Manns@bdvb.de>
2015-05-14 21:41:50 +0200

    Updated user doc

--- lib/PublicanCreators/version.rb
+++ lib/PublicanCreators/version.rb
@@ -32,7 +32,7 @@ class PublicanCreatorsVersion
     # Minor version.
     MINOR = 3
     # Patchlevel
-    PATCH = 2
+    PATCH = 3
     # Buildversion. Mostly 0
     BUILD = 0
     # @note This will be shown by PublicanCreatorsVersion::Version::STRING
