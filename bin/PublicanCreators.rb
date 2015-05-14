97c397f22005daac4c44e0ae42faaadaeb4bcb34
saigkill <Sascha.Manns@bdvb.de>
2015-05-14 21:41:20 +0200

    Updated user doc

--- bin/PublicanCreators.rb
+++ bin/PublicanCreators.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'bundler/setup'
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib', 'PublicanCreators.rb'))
+require 'PublicanCreators'
+#require File.expand_path(File.join(File.dirname(__FILE__), '../lib', 'PublicanCreators.rb'))
 
 # You can add fixtures and/or initialization code here to make experimenting
 # with your gem easier. You can also use a different console, if you like.
@@ -10,4 +11,4 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../lib', 'PublicanCr
 # require "pry"
 # Pry.start
 
-PublicanCreators
+IRB.start
